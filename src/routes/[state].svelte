066cc5396cc09622bdeb148f635926920782dd96
Ariel Mariani <ariel@peermedical.com>
2020-05-27 14:36:01 -0300

    Add base pages and components

---
+++ src/routes/[state].svelte
@@ -0,0 +1,22 @@
+<script context="module">
+  // Run on the server and on the client as well
+  // preload function takes two params (page, session)
+  export async function preload(page, session) {
+    // We get the param "state" because of the filename [state].svelte
+    return {
+      state: page.params["state"]
+    };
+  }
+</script>
+
+<script>
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
+  import TableContainer from "../components/TableContainer.svelte";
+
+  export let state;
+</script>
+
+<h1>{state}</h1>
+<CovidStat />
+<CovidChart />
